1e546a0adffed0fc3d854b5cc4bd543feb91b04f
Handrata <hhandrata@gmail.com>
2025-02-28 20:36:38 +1100

    update mason and treesitter

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -10,7 +8,18 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = {
+        "bashls",
+        "cssls",
+        "dockerls",
+        "eslint", -- javascript
+        "jinja_lsp",
+        "jsonls",
+        "lemminx", -- xml
         "lua_ls",
+        "remark_ls", -- markdown
+        "pyright",
+        "superhtml",
+        "yamlls",
         -- add more arguments for adding more language servers
       },
     },
